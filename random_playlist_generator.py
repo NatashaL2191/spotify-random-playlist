15567dd91a1f6718c9ede430961923c56d02e725
natasha linares <linaresn2024@gmail.com>
2025-08-08 09:41:18 -0400

    Added playlist creation functions

--- random_playlist_generator.py
+++ random_playlist_generator.py
@@ -13,6 +13,20 @@ SCOPE = os.getenv('SPOTIPY_SCOPE')
 
 # Initialize Spotify client
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE))
-#=== Function to Generate Random Playlist ===#
 user = sp.current_user()
 print("Logged in as:", user["display_name"])
+
+#create playlist
+playlist= sp.user_playlist_create(name=playlist_name, public=True, user=user_id)
+
+
+def create_random_playlist(user_id, playlist_name, genre, trackcount(20))
+
+
+
+ # Search for tracks in each genre
+    all_tracks = []
+    for genre in genres:
+        results = sp.search(q=f'genre:"{genre}"', type='track', limit=50)
+        tracks = [item['uri'] for item in results['tracks']['items']]
+        all_tracks.extend(tracks)
