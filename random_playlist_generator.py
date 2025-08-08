ad69f2ff185ea65fb8c1f00534014619531d8dff
natasha linares <linaresn2024@gmail.com>
2025-08-08 09:49:01 -0400

    Completed 'Create_random_playlist' function

--- random_playlist_generator.py
+++ random_playlist_generator.py
@@ -16,13 +16,20 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, clie
 user = sp.current_user()
 print("Logged in as:", user["display_name"])
 
-#create playlist
-playlist= sp.user_playlist_create(name=playlist_name, public=True, user=user_id)
 
 
 def create_random_playlist(user_id, playlist_name, genre, trackcount(20))
 
-
+    """
+    Create a random Spotify playlist from given genres.
+    
+    user_id: Your Spotify username (string)
+    playlist_name: Name of the playlist (string)
+    genres: List of genres to pull songs from
+    track_count: Number of songs in playlist
+        """
+    #create playlist
+    playlist= sp.user_playlist_create(name=playlist_name, public=True, user=user_id)
 
  # Search for tracks in each genre
     all_tracks = []
@@ -30,3 +37,11 @@ def create_random_playlist(user_id, playlist_name, genre, trackcount(20))
         results = sp.search(q=f'genre:"{genre}"', type='track', limit=50)
         tracks = [item['uri'] for item in results['tracks']['items']]
         all_tracks.extend(tracks)
+        
+ # Pick random tracks
+    selected_tracks = random.sample(all_tracks, min(track_count, len(all_tracks)))
+    
+    # Add tracks to playlist
+    sp.playlist_add_items(playlist['id'], selected_tracks)
+    
+    print(f"Playlist '{playlist_name}' created with {len(selected_tracks)} random songs!")
